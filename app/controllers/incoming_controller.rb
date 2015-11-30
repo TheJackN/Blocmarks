f4db9a4f94829baf2784e208112a856b1f9087bb
James <JamesCarlson2@hotmail.com>
2015-11-30 11:19:53 -0600

    Fix incoming controller to eliminate possibility of nil user bookmark creation

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,32 +4,24 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-
-    # You put the message-splitting and business
-    # magic here.
-
-    @user = User.where(email: params[:sender])
-    @topic = Topic.where(title: params[:subject])
+    @user = User.where(email: params[:sender]).first
+    @topic = Topic.find_by(title: params[:subject])
     bookmark_url = params["body-plain"]
 
     if @user.nil?
-      # generated_password = Devise.friendly_token.first(8)
-      # @user = User.create!(:email => params[:sender], :password => generated_password)
-      # # @user.save
-      #
-      # # RegistrationMailer.welcome(@user, generated_password).deliver
       break
-    end
-
-    if @topic.nil?
+    elsif @topic.nil?
       @topic = Topic.create!(:user => @user, :title => params[:subject])
       @topic.save
+      @bookmark = @topic.bookmarks.build(url: bookmark_url)
+      @bookmark.user = @user
+      @bookmark.save
+    else
+      @bookmark = @topic.bookmarks.build(url: bookmark_url)
+      @bookmark.user = @user
+      @bookmark.save
     end
 
-    @bookmark = @topic.bookmarks.build(url: bookmark_url)
-    @bookmark.save
-
-    # Assuming all went well.
     head 200
   end
 end
